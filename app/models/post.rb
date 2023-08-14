b2438c9c51492d6cda9da040a926eda739c02f9a
Alex <alexkimanthi92@gmail.com>
2023-08-14 11:37:06 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validate :validated_title
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    private
+
+    def validated_title
+        allowed_titles = ["You Won't Believe These True Facts"]
+        disallowed_titles = ["True Facts"]
+    
+        if allowed_titles.exclude?(title) && disallowed_titles.include?(title)
+          errors.add(:title, "is not allowed")
+        end
+    end 
 end
